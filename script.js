bfd95f32b2d87261d661bfc3c7d02d20048df522
Ritik-the-dev <joshiritik865@gmail.com>
2024-08-26 23:12:03 +0530

    logic-improved

--- script.js
+++ script.js
@@ -4,25 +4,57 @@ let currentEquation = "";
 
 Array.from(buttons).forEach((e) => {
   e.addEventListener("click", () => {
-    if (e.innerText === "=") {
+    const value = e.innerText;
+
+    if (value === "=") {
       try {
-        currentEquation = eval(currentEquation).toFixed(3).toString();
-        display.value = currentEquation;
+        if (currentEquation !== "") {
+          currentEquation = eval(currentEquation).toFixed(3).toString();
+          display.value = currentEquation;
+        }
       } catch (error) {
         display.value = "Error";
         currentEquation = "";
       }
-    } else if (e.innerText === "RESET") {
+    } else if (value === "RESET") {
       currentEquation = "";
       display.value = currentEquation;
-    } else if (e.innerText === "DEL") {
+    } else if (value === "DEL") {
       currentEquation = currentEquation.slice(0, -1);
       display.value = currentEquation;
-    } else if (e.innerText === "X") {
-      currentEquation += "*";
-      display.value = currentEquation;
+    } else if (value === "X") {
+      if (
+        currentEquation.length !== 0 &&
+        !["+", "-", "*", "/"].includes(currentEquation.slice(-1))
+      ) {
+        currentEquation += "*";
+        display.value = currentEquation;
+      }
+    } else if (["+", "*", "/"].includes(value)) {
+      if (
+        currentEquation.length !== 0 &&
+        !["+", "*", "/"].includes(currentEquation.slice(-1))
+      ) {
+        currentEquation += value;
+        display.value = currentEquation;
+      }
+    } else if (value === "-") {
+      if (
+        currentEquation === "" ||
+        !["-"].includes(currentEquation.slice(-1)) ||
+        ["+", "*", "/"].includes(currentEquation.slice(-1))
+      ) {
+        currentEquation += value;
+        display.value = currentEquation;
+      }
+    } else if (value === ".") {
+      const lastNumber = currentEquation.split(/[\+\-\*\/]/).pop();
+      if (!lastNumber.includes(".")) {
+        currentEquation += value;
+        display.value = currentEquation;
+      }
     } else {
-      currentEquation += e.innerText;
+      currentEquation += value;
       display.value = currentEquation;
     }
   });
