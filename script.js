09673a421cbb69bf751d67a2cf8a3ac690032ad4
Ritik-the-dev <joshiritik865@gmail.com>
2024-08-26 22:47:42 +0530

    Micro-Project3

---
+++ script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById("input-box");
+const buttons = document.getElementsByClassName("calc");
+let currentEquation = "";
+
+Array.from(buttons).forEach((e) => {
+  e.addEventListener("click", () => {
+    if (e.innerText === "=") {
+      try {
+        currentEquation = eval(currentEquation).toString();
+        display.value = currentEquation;
+      } catch (error) {
+        display.value = "Error";
+        currentEquation = "";
+      }
+    } else if (e.innerText === "RESET") {
+      currentEquation = "";
+      display.value = currentEquation;
+    } else if (e.innerText === "DEL") {
+      currentEquation = currentEquation.slice(0, -1);
+      display.value = currentEquation;
+    } else if (e.innerText === "X") {
+      currentEquation += "*";
+      display.value = currentEquation;
+    } else {
+      currentEquation += e.innerText;
+      display.value = currentEquation;
+    }
+  });
+});
